5c9f997eab65925b4c21b1ff0ea14e27b21701e2
Chelsea <chelsea.yang8@gmail.com>
2024-11-02 20:06:07 -0600

    complete

--- app/page.js
+++ app/page.js
@@ -6,6 +6,8 @@ export default function Page(){
       <h1 class="text-2xl">CPRG 306: Web Development 2 - Assignments</h1>
       <br></br>
       <Link href="week-2">Week 2</Link>
+      <br></br>
+      <Link href="week-3">Week 3</Link>
     </main>
   );
 }
